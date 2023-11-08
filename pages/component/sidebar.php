b57332321cdf4da2bb58c98a3ca045cfa5d0bab9
nguyenanhtuan0502 <147410342+nguyenanhtuan0502@users.noreply.github.com>
2023-11-08 14:28:27 -0500

    Add files via upload

--- pages/component/sidebar.php
+++ pages/component/sidebar.php
@@ -7,7 +7,7 @@
     <div class="img-admin d-flex flex-column align-items-center text-center gap-2">
         <img class="rounded-circle" src="../assets/img/img-admin.jpg" alt="img-admin" height="120" width="120">
         <h2 class="h6 fw-bold"><?php echo $_SESSION['name']; ?></h2>
-        <span class="h7 admin-color">admin</span>
+        <span class="h7 admin-color"><?php echo $_SESSION['role']; ?></span>
     </div>
     <div class=" bg-list d-flex flex-column align-items-center fw-bold gap-2 mt-4 ">
         <ul class="d-flex flex-column list-unstyled">
@@ -17,6 +17,7 @@
             <li class="h7"><a class=" nav-link text-dark" href="payment_details.php"><i class="fal fa-usd-square me-2"></i> <span>Payment</span></a></li>
             <li class="h7"><a class=" nav-link text-dark" href="about.php"><i class="fal fa-file-chart-line me-2"></i> <span>About</span></a></li>
             <li class="h7"><a class=" nav-link text-dark" href="contact.php"><i class="fal fa-sliders-v-square me-2"></i> <span>Contact</span></a></li>
+            <li class="h7"><a class=" nav-link text-dark" href="chat.php"><i class="fal fa-sliders-v-square me-2"></i><span>Chat</span></a>
         </ul>
         <ul class="logout d-flex justify-content-start list-unstyled">
             <li class=" h7"><a class="nav-link text-dark" href="../index.php"><span>Logout</span><i class="fal fa-sign-out-alt ms-2"></i></a></li>
